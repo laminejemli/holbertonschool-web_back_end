995a8b6e19e795c6d9da2ec9c8a9c39505eeddd8
laminejemli <70071258+laminejemli@users.noreply.github.com>
2022-04-11 02:00:37 +0200

    Delete 0x00-python_variable_annotations directory

--- 0x00-python_variable_annotations/3-to_str.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Basic annotations task 3"""
-
-
-def to_str(n: float) -> str:
-    """Converts a float to a string
-    """
-    return str(n)
--- 0x00-python_variable_annotations/README.md
+++
@@ -1,1 +0,0 @@
-py variable annotations
