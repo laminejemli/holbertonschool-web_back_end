e41884556b3b1ab1f15282067c485babea3b62ba
Lamine Mokhtar Jemli <70071258+laminejemli@users.noreply.github.com>
2022-05-28 09:59:05 +0200

    Update 0-get_list_students.js

--- 0x11-ES6_data_manipulation/0-get_list_students.js
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,9 @@
-export default function getListStudents() {
-    const objArray = [
-      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-      { id: 2, firstName: 'James', location: 'Columbia' },
-      { id: 5, firstName: 'Serena', location: 'San Francisco' },
-    ];
-    return objArray;
-  }
+function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
+
+export default getListStudents;
