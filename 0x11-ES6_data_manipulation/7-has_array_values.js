3bdf6e09a2fe9b921dca92bb2a8f57d2a959f055
Lamine Mokhtar Jemli <70071258+laminejemli@users.noreply.github.com>
2022-05-28 10:12:13 +0200

    Update 7-has_array_values.js

--- 0x11-ES6_data_manipulation/7-has_array_values.js
+++ 0x11-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,5 @@
-export default function hasValuesFromArray(set, array) {
+function hasValuesFromArray(set, array) {
     return array.every((val) => set.has(val));
   }
+
+export default hasValuesFromArray;
