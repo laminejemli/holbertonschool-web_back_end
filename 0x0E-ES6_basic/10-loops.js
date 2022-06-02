3a4a9d87842f47738d373352d3133bc7b2195f4c
Lamine Mokhtar Jemli <70071258+laminejemli@users.noreply.github.com>
2022-06-03 00:21:32 +0200

    Delete 0x0E-ES6_basic directory

--- 0x0E-ES6_basic/10-loops.js
+++
@@ -1,8 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-    const Array = [];
-    for (const idx of array) {
-      Array.push(appendString + idx);
-    }
-  
-    return Array;
-  }
--- 0x0E-ES6_basic/11-createEmployeesObject.js
+++
@@ -1,6 +0,0 @@
-export default function createEmployeesObject(departmentName, employees) {
-    const newObject = {
-      [departmentName]: [...employees],
-    };
-    return newObject;
-  }
